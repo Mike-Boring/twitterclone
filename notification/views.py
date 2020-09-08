76af26ae9406db432490819ba90d4624c0227934
Mike Boring <shekinah999@msn.com>
2020-09-08 17:13:50 -0600

    Connected a couple missing info links

--- notification/views.py
+++ notification/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 
 from notification.models import Notification
 
+from twitteruser.models import Relationship
 
 from tweet.models import Tweet
 
 
 def notification_view(request, user_id):
     number_tweets = len(Tweet.objects.filter(twitter_user=request.user))
+    if Relationship.objects.filter(from_person=request.user):
+        number_following = len(
+            Relationship.objects.filter(from_person=request.user))
+
+    else:
+        number_following = 0
     user_notifications = Notification.objects.filter(
         tweeted_user=request.user)
     if len(user_notifications) > 0:
@@ -28,5 +35,6 @@ def notification_view(request, user_id):
          "number_tweets": number_tweets,
          "profile_user": request.user,
          "number_notifications": number_notifications,
+         "number_following": number_following,
          "delete_info": delete_info}
     )
